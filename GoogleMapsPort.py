c796bcd6a3dbcd2f6a0557a613a34ac0145f5438
nbrilha1 <37334506+nbrilha1@users.noreply.github.com>
2019-11-05 09:56:05 -0500

    Update GoogleMapsPort.py
    Changed lat and long variable names to latitude and longitude respectively.

--- GoogleMapsPort.py
+++ GoogleMapsPort.py
@@ -3,14 +3,14 @@ import json
 import requests
 
 API_key = "&key=AIzaSyDjxQG1nLTRjlCFbVB4mq_jMtu40GMR5D4"
-lat = 39.254750
-long = -76.710840
+latitude = 39.254750
+longitude = -76.710840
 radial_dist = 50
 API_base_url = "https://maps.googleapis.com/maps/api/"
 textsearch = "place/textsearch/json?query="
 #search_term = input("Enter Search Term: ")
 r = requests.get(API_base_url + textsearch + "Schools" + "&location=" +
-                 str(lat) + "," + str(long) + "&radius=" + str(radial_dist)
+                 str(latitude) + "," + str(longitude) + "&radius=" + str(radial_dist)
                  + API_key)
 r = r.json()
 print("content-type: text/html\n\n" )
