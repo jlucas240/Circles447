05be45c7e794412a1997192f81755183980cfbca
nbrilha1 <37334506+nbrilha1@users.noreply.github.com>
2019-11-07 09:27:17 -0500

    Update GoogleMapsPort.py
    Commented out development layout for GoogleMapsPort.py.

--- GoogleMapsPort.py
+++ GoogleMapsPort.py
@@ -5,14 +5,41 @@ import requests
 API_key = "&key=AIzaSyDjxQG1nLTRjlCFbVB4mq_jMtu40GMR5D4"
 latitude = 39.254750
 longitude = -76.710840
-radial_dist = 50
+radial_dist = 25
 API_base_url = "https://maps.googleapis.com/maps/api/"
 textsearch = "place/textsearch/json?query="
 #search_term = input("Enter Search Term: ")
+
+#Takes in input json to modify
+#latitude, longitude, radial_dist, and other variables
+radial_dist = state[0]
+#get longitude and attiude from address
+#state[1] Address
+#state[2] City
+#state[3] State
+#state[4] Zip?
+
+# collect and comine results for each true boolean in state[5] through state[15]
+# 5 Supermarket 
+# 6 School 
+# 7 Churches 
+# 8 Community 
+# 9 Libraries 
+# 10 Cafe 
+# 11 Dance 
+# 12 Gyms 
+# 13 Swimming 
+# 14 Playground 
+# 15 Parks
 r = requests.get(API_base_url + textsearch + "Schools" + "&location=" +
                  str(latitude) + "," + str(longitude) + "&radius=" + str(radial_dist)
                  + API_key)
 r = r.json()
+
+#Send and Get Request
+
 print("content-type: text/html\n\n" )
 for i in r["results"]:
     print("<br><B>"+i["name"]+"</B>")
+
+#Return json
